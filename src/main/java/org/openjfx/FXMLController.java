c6492707afd72a927286047f9a7e847aeeaa4377
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-16 18:18:02 +0100

    Add UI through Scene builder

--- src/main/java/org/openjfx/FXMLController.java
+++ src/main/java/org/openjfx/FXMLController.java
@@ -1,21 +1,20 @@
 package org.openjfx;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class FXMLController implements Initializable {
 
     @FXML
-    private Label label;
+    private ImageView imageView;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     }
 }
