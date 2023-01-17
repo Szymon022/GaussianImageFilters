ebb35dcfe7a21b99b27fc6dd0c29a45d9647700a
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-17 12:56:57 +0100

    Add ImageLoader class

---
+++ src/main/java/org/openjfx/loader/ImageLoader.java
@@ -0,0 +1,29 @@
+package org.openjfx.loader;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageLoader {
+
+    public static Image load(File image) {
+        try {
+            var bufferedImage = ImageIO.read(image);
+            var clonedImage = deepClone(bufferedImage);
+            return SwingFXUtils.toFXImage(clonedImage, null);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static BufferedImage deepClone(BufferedImage bufferedImage) {
+        var colorModel = bufferedImage.getColorModel();
+        var isAlphaPremultiplied = bufferedImage.isAlphaPremultiplied();
+        var raster = bufferedImage.getRaster();
+        return new BufferedImage(colorModel, raster, isAlphaPremultiplied, null);
+    }
+}
