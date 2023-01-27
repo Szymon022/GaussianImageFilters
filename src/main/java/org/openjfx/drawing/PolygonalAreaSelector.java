20c13f9d9f7591946c4b51c2602be00296ea45ed
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-27 22:50:49 +0100

    Implement PolygonalAreaSelector properly

--- src/main/java/org/openjfx/drawing/PolygonalAreaSelector.java
+++ src/main/java/org/openjfx/drawing/PolygonalAreaSelector.java
@@ -5,39 +5,59 @@ import org.openjfx.area.Area;
 import org.openjfx.area.PolygonalArea;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class PolygonalAreaSelector implements AreaSelector {
 
-    private List<Point2D> vertices;
+    private LinkedList<Point2D> vertices;
+    private boolean isClosed;
 
     public PolygonalAreaSelector() {
         vertices = new LinkedList<>();
+        isClosed = false;
     }
 
     @Override
     public Area get() {
-        return new PolygonalArea(vertices);
+        return new PolygonalArea(vertices, isClosed);
     }
 
     @Override
     public void addPoint(int x, int y) {
-        if (noSuchVertexExist(x, y)) {
+        if (isClosed) return;
+        if (isOriginVertex(x, y) && vertices.size() > 2) {
+            isClosed = true;
+        } else if (noSuchVertexExist(x, y)) {
             vertices.add(new Point2D(x, y));
         }
     }
 
     @Override
-    public void moveBy(int x, int y) {
-        vertices = vertices.stream()
-                .map((point) -> {
-                    int pointX = (int) point.getX();
-                    int pointY = (int) point.getY();
-                    return new Point2D(pointX + x, pointY + y);
-                }).toList();
+    public void moveTo(int x, int y) {
+        if (vertices.isEmpty()) return;
+        var origin = vertices.get(0);
+        var xShift = x - origin.getX();
+        var yShift = y - origin.getY();
+        vertices = shiftVerticesBy(vertices, xShift, yShift);
     }
 
     private boolean noSuchVertexExist(int x, int y) {
         return vertices.stream().noneMatch((point) -> (int) point.getX() == x && (int) point.getY() == y);
+    }
+
+    private boolean isOriginVertex(int x, int y) {
+        if (vertices.size() == 0) return false;
+        var threshold = 5;
+        var originVertex = vertices.get(0);
+        var originX = (int) originVertex.getX();
+        var originY = (int) originVertex.getY();
+        return Math.abs(x - originX) <= threshold && Math.abs(y - originY) <= threshold;
+    }
+
+    private LinkedList<Point2D> shiftVerticesBy(LinkedList<Point2D> vertices, double xShift, double yShift) {
+        var shiftedVertices = new LinkedList<Point2D>();
+        for (Point2D vertex : vertices) {
+            shiftedVertices.add(new Point2D(vertex.getX() + xShift, vertex.getY() + yShift));
+        }
+        return shiftedVertices;
     }
 }
