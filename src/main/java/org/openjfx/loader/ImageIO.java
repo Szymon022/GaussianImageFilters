0e93139250106fbad16818de614ee8b38b0f4812
Szymon022 <szymon.kurpas00@gmail.com>
2023-01-28 10:06:24 +0100

    Add file saving

--- src/main/java/org/openjfx/loader/ImageIO.java
+++ src/main/java/org/openjfx/loader/ImageIO.java
@@ -3,20 +3,28 @@ package org.openjfx.loader;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.WritableImage;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class ImageLoader {
+public class ImageIO {
 
     public static WritableImage load(File image) {
         try {
-            var bufferedImage = ImageIO.read(image);
+            var bufferedImage = javax.imageio.ImageIO.read(image);
             var clonedImage = deepClone(bufferedImage);
             return SwingFXUtils.toFXImage(clonedImage, null);
         } catch (IOException e) {
             return null;
+        }
+    }
+
+    public static void save(WritableImage image, File targetFile) {
+        try {
+            var bufferedImage = SwingFXUtils.fromFXImage(image, null);
+            javax.imageio.ImageIO.write(bufferedImage, "png", targetFile);
+        } catch (IOException e) {
+
         }
     }
 
--- src/main/java/org/openjfx/transformer/ImageTransformer.java
+++ src/main/java/org/openjfx/transformer/ImageTransformer.java
@@ -7,7 +7,7 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import org.openjfx.area.Area;
 import org.openjfx.kernels.Kernel;
-import org.openjfx.loader.ImageLoader;
+import org.openjfx.loader.ImageIO;
 
 import java.io.File;
 
@@ -20,8 +20,8 @@ public class ImageTransformer {
     private double height;
 
     public ImageTransformer(ImageTransformer.Builder imageTransformerBuilder) {
-        this.image = ImageLoader.load(imageTransformerBuilder.imageFile);
-        this.readOnlyImage = ImageLoader.load(imageTransformerBuilder.imageFile);
+        this.image = ImageIO.load(imageTransformerBuilder.imageFile);
+        this.readOnlyImage = ImageIO.load(imageTransformerBuilder.imageFile);
         this.kernel = imageTransformerBuilder.kernel;
         assert image != null;
         width = image.getWidth();
